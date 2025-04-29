3fde996d8286a037c63d3d105ed42c4004f582e9
Dai <dai.workcontact@gmail.com>
2025-04-29 13:54:33 +0700

    Changed the port to work with Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,5 +9,6 @@ def home():
     return "Hello from Flask App running via DevOps CI/CD!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Fallback to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
 
